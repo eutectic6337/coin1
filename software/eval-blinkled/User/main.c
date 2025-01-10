bdedf34c9e9adbe2abac78339c28f1efa72feda8
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-01-09 19:28:38 -0600

    missed a pin

--- software/eval-blinkled/User/main.c
+++ software/eval-blinkled/User/main.c
@@ -10,7 +10,7 @@ void GPIO_Toggle_INIT(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 

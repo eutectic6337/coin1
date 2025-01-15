a35d6d9c3ccdb5ccb83c9b3b4e88d477069752b1
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-01-14 19:38:46 -0600

    use PC3,PC2 = LED1,LED2 on coin

--- software/eval-blinkled/User/main.c
+++ software/eval-blinkled/User/main.c
@@ -6,13 +6,11 @@ void GPIO_Toggle_INIT(void)
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_2;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
 int main(void)
@@ -23,18 +21,18 @@ int main(void)
 
 	GPIO_Toggle_INIT();
 
-	int LED1 = 0;
-    int LED2 = 0;
+	int fLED1 = 0;
+    int fLED2 = 0;
 
     while(1)
     {
         Delay_Ms(100);
 
-        GPIO_WriteBit(GPIOC, GPIO_Pin_2, LED1 = !LED1);
-        if (LED1) {
+        GPIO_WriteBit(GPIOC, GPIO_Pin_3, fLED1 = !fLED1);
+        if (fLED1) {
 			Delay_Ms(100);
 
-			GPIO_WriteBit(GPIOD, GPIO_Pin_3, LED2 = !LED2);
+			GPIO_WriteBit(GPIOC, GPIO_Pin_2, fLED2 = !fLED2);
         }
     }
 }

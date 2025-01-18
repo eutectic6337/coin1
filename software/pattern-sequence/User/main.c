6311b40e3d3dc9f16d616e37999affc8cac50d3b
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-01-17 18:54:00 -0600

    skeleton for pattern sequence

--- software/pattern-sequence/User/main.c
+++ software/pattern-sequence/User/main.c
@@ -1,7 +1,10 @@
 #include "debug.h"
 
+#define NUM_ARRAY_ELEMS(a) ((sizeof a)/(sizeof a[0]))
+
+#define NUM_LEDS 10
 struct pattern {
-	int bright[10];// percentage of full
+	int bright[NUM_LEDS];// percentage of full
 	int time;// milliseconds
 };
 
@@ -72,15 +75,23 @@ void adjust_LED(int n, int target) {
 }
 
 void LED_on(int n) {
-	if (n < 1 || n > 10) return;
+	if (n < 1 || n > NUM_LEDS) return;
 	adjust_LED(n, 1);
 }
 void LED_off(int n) {
-	if (n < 1 || n > 10) return;
+	if (n < 1 || n > NUM_LEDS) return;
 	adjust_LED(n, 0);
 }
 
-void GPIO_Toggle_INIT(void)
+
+int get_current_time_ms(void)
+{
+	//FIXME
+	return 0;
+}
+
+
+void setup_LEDs(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
@@ -95,6 +106,50 @@ void GPIO_Toggle_INIT(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    //FIXME: PWM
+}
+void loop_LEDs(void)
+{
+	//FIXME: PWM
+}
+void set_LED_brightness(int led, int bright)
+{
+	//FIXME: PWM
+}
+
+
+struct pattern *sequence_start;
+int sequence_length;
+
+int sequence_step;
+int next_step_ms;
+
+void setup_sequence(void)
+{
+	sequence_start = chase;
+	sequence_length = NUM_ARRAY_ELEMS(chase);
+}
+void loop_sequence(void)
+{
+	if (!sequence_start) return;
+
+	int current_time = get_current_time_ms();
+	if (current_time >= next_step_ms) {
+		if (sequence_length > sequence_step) {
+			sequence_step++;
+		}
+		else {
+			sequence_step = 0;
+		}
+
+		struct pattern *s = sequence_start+sequence_step;
+		next_step_ms = current_time + s->time;
+
+		for (int i = 0; i < NUM_LEDS; i++) {
+			set_LED_brightness(i, s->bright[i]);
+		}
+	}
 }
 
 int main(void)
@@ -103,10 +158,11 @@ int main(void)
 	SystemCoreClockUpdate();
 	Delay_Init();
 
-	GPIO_Toggle_INIT();
-
+	setup_LEDs();
+	setup_sequence();
     while(1)
     {
-    	run_sequence();
+    	loop_sequence();
+    	loop_LEDs();
     }
 }

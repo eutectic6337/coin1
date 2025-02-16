877a6ce4446b42083f10628919c106c8319bd7f1
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-02-16 14:15:56 -0600

    add comments; drop in timer interrupt handler

--- software/validate-ideas/User/main.c
+++ software/validate-ideas/User/main.c
@@ -1,11 +1,17 @@
 /*
  * clock/timing?
+ * - use timer interrupt to update microseconds counter
+ * - locking for exclusive access?
  *
  * Capacitive sensing input: PA2
  *
  * back2back LEDs
  * PWM output:PD2af aka T1C1
  * GPIO output PD3
+ * - success: back-to-back LEDs in parallel across PD2/PD3 blink alternately
+ *   per PD3, one at brightness set by T1C1 and one at inverse brightness
+ * - to test: can switch PD3 to hi-Z for no LEDs lit, and switch back to hi/lo
+ * - to test: can adjust T1C1 duty cycle in flight
  *
  * sleep/wakeup
  */
@@ -46,6 +52,78 @@ void loop_clock(void)
 		}
 	}
 }
+////////
+/*
+ *@Note
+ *TIMER_INT
+ *MODE 1 Timer count triggers update interrupt. Updata_time = psc*arr*RepetitionCounter/system
+ *MODE 2 generates an update event and triggers an update interrupt.
+ *
+ */
+
+
+/* Mode Definition */
+#define MODE1   0
+#define MODE2   1
+
+/* Mode Selection */
+#define MODE MODE1
+//#define MODE MODE2
+
+void TIM1_UP_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_UP_IRQHandler(void)
+{
+    if(TIM_GetITStatus(TIM1, TIM_IT_Update)==SET)
+    {
+        printf("--------updata\r\n");
+    }
+    TIM_ClearITPendingBit( TIM1, TIM_IT_Update );
+}
+
+void TIM1_INT_Init( u16 arr, u16 psc)
+{
+
+    NVIC_InitTypeDef NVIC_InitStructure={0};
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure={0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE );
+
+    TIM_TimeBaseInitStructure.TIM_Period = arr;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 50;
+    TIM_TimeBaseInit( TIM1, &TIM_TimeBaseInitStructure);
+
+    TIM_ClearITPendingBit( TIM1, TIM_IT_Update );
+
+    NVIC_InitStructure.NVIC_IRQChannel =TIM1_UP_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority =0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority =1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd =ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+
+}
+
+int main(void)
+{
+
+    TIM1_INT_Init( 100-1, 48000-1);
+#if(MODE==MODE1)
+
+    TIM_Cmd( TIM1, ENABLE );//5S
+
+#elif(MODE==MODE2)
+
+    TIM_GenerateEvent(TIM1, TIM_IT_Update);
+
+#endif
+}
+
+////////
+
 
 void init_capsense(void)
 {
@@ -133,24 +211,20 @@ void init_LEDout(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
     GPIO_InitTypeDef GPIO_InitStructure = {
-    		.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_2,
+    		.GPIO_Pin = GPIO_Pin_3,
 			.GPIO_Mode = GPIO_Mode_Out_PP,
 			.GPIO_Speed = GPIO_Speed_30MHz
     };
     GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    //init_TIM1_PWMOut(100, 48000-1, 50);
+    init_TIM1_PWMOut(100, 4800-1, 1);
 }
 void loop_LEDout(void)
 {
-	GPIO_WriteBit(GPIOD, GPIO_Pin_2, 1);
-	Delay_Ms(1);
 	GPIO_WriteBit(GPIOD, GPIO_Pin_3, 1);
-	Delay_Ms(2);
-	GPIO_WriteBit(GPIOD, GPIO_Pin_2, 0);
-	Delay_Ms(3);
+	Delay_Ms(200);
 	GPIO_WriteBit(GPIOD, GPIO_Pin_3, 0);
-	Delay_Ms(4);
+	Delay_Ms(300);
 }
 
 void init_sleepwake(void)
@@ -174,7 +248,7 @@ void loop(void)
 {
 	static int a = 0;
 	static int b = 0;
-	if (++a > 999) {
+	if (++a > 2) {
 		a = 0;
     	printf("%d\t%lu-%u-%u\r\n", b++,
     			(unsigned long)seconds, (unsigned)millis, (unsigned)micros);

4afc988f74bc46a7f4bd07a5ffad9ac3a1781160
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-02-22 10:06:58 -0600

    start making config switchable

--- software/validate-ideas/User/pwmleds.c
+++ software/validate-ideas/User/pwmleds.c
@@ -7,6 +7,7 @@
 #include <ch32v00x.h>
 #include "pwmleds.h"
 
+#ifdef USE_PWM
 /* PWM Output Mode Selection */
 #define PWM_MODE1   0
 #define PWM_MODE2   1
@@ -49,10 +50,14 @@ static void init_TIM1_PWMOut(u16 period, u16 prescaler, u16 pulsewidth)
     TIM_ARRPreloadConfig( TIM1, ENABLE );
     TIM_Cmd( TIM1, ENABLE );
 }
+#endif
 
 void init_pwmleds(void)
 {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+#ifdef USE_PWM
+    //init_TIM1_PWMOut(100, 4800-1, 20);
+#else
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
     GPIO_InitTypeDef GPIO_InitStructure = {
     		.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_2,
@@ -60,12 +65,13 @@ void init_pwmleds(void)
 			.GPIO_Speed = GPIO_Speed_30MHz
     };
     GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    //init_TIM1_PWMOut(100, 4800-1, 20);
+#endif
 }
 
 #include "systick.h"
 
+#ifdef USE_PWM
+#else
 #define PD2_HIGH_MS 100u
 #define PD2_LOW_MS 300u
 
@@ -77,9 +83,13 @@ static timestamp next_PD2_low;
 
 static timestamp next_PD3_high;
 static timestamp next_PD3_low;
+#endif
+
 void loop_pwmleds(void)
 {
 	u32 now = millis();
+#ifdef USE_PWM
+#else
 	if (!next_PD2_high) next_PD2_high = now;
 	if (!next_PD2_low) next_PD2_low = now + PD2_HIGH_MS;
 	if (!next_PD3_high) next_PD3_high = now;
@@ -101,4 +111,5 @@ void loop_pwmleds(void)
 		GPIO_WriteBit(GPIOD, GPIO_Pin_3, 0);
 		next_PD3_low += PD3_HIGH_MS + PD3_LOW_MS;
 	}
+#endif
 }

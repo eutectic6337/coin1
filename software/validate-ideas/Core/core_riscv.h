789c8e5176850970d5e68c5d6d8d6966350b0235
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-02-21 18:53:33 -0600

    make inline functions ISO rather than K&R (or C++)

--- software/validate-ideas/Core/core_riscv.h
+++ software/validate-ideas/Core/core_riscv.h
@@ -122,7 +122,7 @@ typedef struct
  *
  * @return  none
  */
-__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __enable_irq()
+__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __enable_irq(void)
 {
   __asm volatile ("csrs mstatus, %0" : : "r" (0x88) );
 }
@@ -135,7 +135,7 @@ __attribute__( ( always_inline ) ) RV_STATIC_INLINE void __enable_irq()
  *
  * @return  none
  */
-__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __disable_irq()
+__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __disable_irq(void)
 {
   __asm volatile ("csrc mstatus, %0" : : "r" (0x88) );
 }
@@ -147,7 +147,7 @@ __attribute__( ( always_inline ) ) RV_STATIC_INLINE void __disable_irq()
  *
  * @return  none
  */
-__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __NOP()
+__attribute__( ( always_inline ) ) RV_STATIC_INLINE void __NOP(void)
 {
   __asm volatile ("nop");
 }

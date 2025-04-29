eef7b600c1bc50f659b1f4c512ee52f6df73f883
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-04-29 16:14:09 -0500

    new sample code: first, use SWD for IO

---
+++ software/10-swdio/User/main.c
@@ -0,0 +1,17 @@
+/* SWD serial I/O */
+#include "debug.h"
+
+int main(void)
+{
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    SystemCoreClockUpdate();
+    Delay_Init();
+    SDI_Printf_Enable();
+    printf("SystemClk:%d\r\n",SystemCoreClock);
+    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+
+    while(1)
+    {
+
+    }
+}

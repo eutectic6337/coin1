91be4fc7264011adcdc58449e238cf2f20b11718
mlp <118296511+eutectic6337@users.noreply.github.com>
2025-08-01 12:24:21 -0500

    re-roll the loop, still works

--- software/Blink/Blink.ino
+++ software/Blink/Blink.ino
@@ -1,147 +1,48 @@
 /*
   Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://docs.arduino.cc/hardware/
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
 
-// the setup function runs once when you press reset or power the board
+#define NUM_ARRAY_ELEMS(a) ((sizeof a)/(sizeof a[0]))
+
+struct pin_pair {	int p1, p2;	}
+const LED_pair_pins[] = {
+		{PC5, PC3/*TIM1_CH3*/},
+		{PC2, PC0/*TIM2_CH3*/},
+		{PC1, PA1/*TIM1_CH2*/},
+		{PD0, PD7/*TIM2_CH4*/},
+		{PD5, PD4/*TIM2_CH1*/},
+		{PD1/*SWIO*/, PD3/*TIM2_CH2*/},
+		{PC7, PD2/*TIM1_CH1*/},
+		{PC6, PC4/*TIM1_CH4*/},
+};
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PC5, OUTPUT);
-  pinMode(PC3, OUTPUT);
-  pinMode(PC2, OUTPUT);
-  pinMode(PC0, OUTPUT);
-  pinMode(PC1, OUTPUT);
-  pinMode(PA1, OUTPUT);
-  pinMode(PD0, OUTPUT);
-  pinMode(PD7, OUTPUT);
-  pinMode(PD5, OUTPUT);
-  pinMode(PD4, OUTPUT);
-  pinMode(PD1, OUTPUT);
-  pinMode(PD3, OUTPUT);
-  pinMode(PC7, OUTPUT);
-  pinMode(PD2, OUTPUT);
-  pinMode(PC6, OUTPUT);
-  pinMode(PC4, OUTPUT);
+  delay(3000);
+	for (int i = 0; i < NUM_ARRAY_ELEMS(LED_pair_pins); i++) {
+		pinMode(LED_pair_pins[i].p1, OUTPUT);
+		pinMode(LED_pair_pins[i].p2, OUTPUT);
+
+		digitalWrite(LED_pair_pins[i].p1, LOW);
+		digitalWrite(LED_pair_pins[i].p2, LOW);
+	}
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(PC5, LOW);
-  delay(100);
-  digitalWrite(PC3, LOW);
-  delay(100);
-  digitalWrite(PC2, LOW);
-  delay(100);
-  digitalWrite(PC0, LOW);
-  delay(100);
-  digitalWrite(PC1, LOW);
-  delay(100);
-  digitalWrite(PA1, LOW);
-  delay(100);
-  digitalWrite(PD0, LOW);
-  delay(100);
-  digitalWrite(PD7, LOW);
-  delay(100);
-  digitalWrite(PD5, LOW);
-  delay(100);
-  digitalWrite(PD4, LOW);
-  delay(100);
-  digitalWrite(PD1, LOW);
-  delay(100);
-  digitalWrite(PD3, LOW);
-  delay(100);
-  digitalWrite(PC7, LOW);
-  delay(100);
-  digitalWrite(PD2, LOW);
-  delay(100);
-  digitalWrite(PC6, LOW);
-  delay(100);
-  digitalWrite(PC4, LOW);
-  delay(100);
-
-  digitalWrite(PC5, HIGH);
-  delay(100);
-  digitalWrite(PC2, HIGH);
-  delay(100);
-  digitalWrite(PC1, HIGH);
-  delay(100);
-  digitalWrite(PD0, HIGH);
-  delay(100);
-  digitalWrite(PD5, HIGH);
-  delay(100);
-  digitalWrite(PD1, HIGH);
-  delay(100);
-  digitalWrite(PC7, HIGH);
-  delay(100);
-  digitalWrite(PC6, HIGH);
-  delay(100);
-
-  digitalWrite(PC5, LOW);
-  delay(100);
-  digitalWrite(PC3, LOW);
-  delay(100);
-  digitalWrite(PC2, LOW);
-  delay(100);
-  digitalWrite(PC0, LOW);
-  delay(100);
-  digitalWrite(PC1, LOW);
-  delay(100);
-  digitalWrite(PA1, LOW);
-  delay(100);
-  digitalWrite(PD0, LOW);
-  delay(100);
-  digitalWrite(PD7, LOW);
-  delay(100);
-  digitalWrite(PD5, LOW);
-  delay(100);
-  digitalWrite(PD4, LOW);
-  delay(100);
-  digitalWrite(PD1, LOW);
-  delay(100);
-  digitalWrite(PD3, LOW);
-  delay(100);
-  digitalWrite(PC7, LOW);
-  delay(100);
-  digitalWrite(PD2, LOW);
-  delay(100);
-  digitalWrite(PC6, LOW);
-  delay(100);
-  digitalWrite(PC4, LOW);
-  delay(100);
-
-  digitalWrite(PC3, HIGH);
-  delay(100);
-  digitalWrite(PC0, HIGH);
-  delay(100);
-  digitalWrite(PA1, HIGH);
-  delay(100);
-  digitalWrite(PD7, HIGH);
-  delay(100);
-  digitalWrite(PD4, HIGH);
-  delay(100);
-  digitalWrite(PD3, HIGH);
-  delay(100);
-  digitalWrite(PD2, HIGH);
-  delay(100);
-  digitalWrite(PC4, HIGH);
-  delay(100);
+ 	for (int i = 0; i < NUM_ARRAY_ELEMS(LED_pair_pins); i++) {
+		digitalWrite(LED_pair_pins[i].p1, HIGH);
+    delay(100);
+  }
+ 	for (int i = 0; i < NUM_ARRAY_ELEMS(LED_pair_pins); i++) {
+		digitalWrite(LED_pair_pins[i].p2, HIGH);
+    delay(100);
+  }
+ 	for (int i = 0; i < NUM_ARRAY_ELEMS(LED_pair_pins); i++) {
+		digitalWrite(LED_pair_pins[i].p1, LOW);
+    delay(200);
+  }
+ 	for (int i = 0; i < NUM_ARRAY_ELEMS(LED_pair_pins); i++) {
+		digitalWrite(LED_pair_pins[i].p2, LOW);
+    delay(200);
+  }
 }
